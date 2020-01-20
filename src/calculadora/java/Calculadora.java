20e799d66f332e5826dc9594b8d5b71da5e14b3b
Antonio Carlos Fischer Nunes da Silva <cacaujr@live.com>
2020-01-20 16:38:44 -0200

    Retirada de imports e variáveis não utilizados

--- src/calculadora/java/Calculadora.java
+++ src/calculadora/java/Calculadora.java
@@ -7,19 +7,12 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.ImageIcon;
 import java.awt.Color;
 
 public class Calculadora extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField numbersCamp;
 	int layers[] = {0,0,0};
 	int campOne;
 	int campTwo;
